b5ef1357e85d07a867e5cca45a5ad933ff97a4d9
alex-mcknight11 <alex.mcknight11@gmail.com>
2022-01-26 11:42:50 -0800

    add score fail state
    Co-Authored-By: Hayley M <hayleymcvay8@gmail.com>

---
+++ README.md
@@ -0,0 +1,12 @@
+#### Describe: rollDice()
+
+Test: "It should return a random number between 1-6, setting a parameter that the number 5 + 1."
+Code:
+rollDice(5);
+Expected Output: 3
+
+Test: "It should return true if the inputted string begins with the numbers 4, 5, 6, 34, or 37"
+Code:
+const text = "3497912082710";
+ccValidator(text);
+Expected Output: true
--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,27 @@
 
 function Player(turn) {
 	this.roll = 0;
-	this.temporaryScore = 0;
+	this.tempScore = 0;
 	this.totalscore = 0;
 	this.turn = turn;
+	this.playerName;
 }
 
 let rollDice = function () {
 	return Math.floor(Math.random() * 6) + 1;
 };
 console.log(rollDice(5));
+this.roll = rollDice(5);
+
+Player.prototype.rollsOne = function () {
+	if (this.roll === 1) {
+		this.tempScore = 0;
+		alert('You rolled a 1 and now your score is 0');
+	} else {
+		this.tempscore += this.roll;
+	}
+};
+
+//if (Player.rolls === 1) {
+//temporaryScore = 0;
+//}
