11218fc82aa7c183c6e4aed335be7140a23d2234
hmcvay <hayleymcvay8@gmail.com>
2022-01-27 14:08:27 -0800

    Add all button functionality and baseline UI.
    Co-Authored-By: Alex McKnight <alex.mcknight11@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+
 // BUSINESS LOGIC
 
 function Player() {
@@ -7,7 +8,7 @@ function Player() {
 }
 
 let rollDice = function () {
-	return Math.floor(Math.random() * 6) + 1;
+	return Math.floor((Math.random() * 6) + 1);
 };
 
 Player.prototype.rollPoints = function () {
@@ -15,7 +16,7 @@ Player.prototype.rollPoints = function () {
 		this.tempScore = 0;
 		alert('You rolled a 1 and now your score is 0');
 	} else {
-		this.tempScore = this.tempScore + this.roll;
+		this.tempScore = this.roll + this.tempScore;
 	}
 };
 
@@ -24,24 +25,55 @@ Player.prototype.hold = function () {
 	this.tempScore = 0;
 };
 
-// Player.prototype.singleRoll = function () {
-// 	this.roll = rollDice();
-// 	this.rollPoints();
-// };
-
-
+Player.prototype.winner = function () {
+	if (this.totalScore >= 100) {
+		alert ('Player 1 wins!');
+	} //else { this.totalScore += this.tempScore;
+	//}
+};
 let player1 = new Player();
 let player2 = new Player();
 
+
+// UI LOGIC
+
+// Player 1
 $(document).ready(function () {
 	$('button#rollP1').click(function (event) {
 		event.preventDefault();
 		player1.roll = rollDice();
-		$('#resultsPlayer1').text(rollDice());
 		player1.rollPoints();
+		$(".roll1").html(player1.roll);
+		$(".turnTotal1").html(player1.tempScore);
+		// $('#p1').append("<li>" + player1.roll + "</li>")
+		console.log(player1.roll);
 	});
-	console.log(player1.roll);
-});
-// UI LOGIC
+	
+	$("button#holdP1").click(function (event) {
+		event.preventDefault();
+		player1.hold();
+		player1.winner();
+		$(".turnTotal1").html("0");
+		$(".roll1").html("0");
+		$(".grandTotal1").html(player1.totalScore);
+	})
 
-//$("#rollDisplay").text(player1.tempScore);
+// Player 2
+	$('button#rollP2').click(function (event) {
+		event.preventDefault();
+		player2.roll = rollDice();
+		player2.rollPoints();
+		$(".roll2").html(player2.roll);
+		$(".turnTotal2").html(player2.tempScore);
+		console.log(player2.roll);
+	});
+	
+	$("button#holdP2").click(function (event) {
+		event.preventDefault();
+		player2.hold();
+		player2.winner();
+		$(".turnTotal2").html("0");
+		$(".roll2").html("0");
+		$(".grandTotal2").html(player2.totalScore);
+	})
+});
